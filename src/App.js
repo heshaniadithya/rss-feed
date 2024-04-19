f3e38a84d5a988fd91efa2f4dd2ae3861e24c25c
heshaniadithya <heshaniadithya2006@gmail.com>
2024-04-19 18:52:45 +0530

    rss-feed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Feed from "./Feed";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  console.log(articles);
+
+  const getArticles = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/")
+      setArticles(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getArticles();
+  }, [])
+
+  console.log(articles);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1 className="text-xl font-semibold text-center mt-5">05 - RSS Feed</h1>
+      <h2 className="text-3xl font-semibold text-center mt-2 mb-4">Good morning, Heshani</h2>
+      <div className="w-3/4 max-w-lg border mx-auto p-5 rounded-xl">
+     <div className="bg-black flex flex-col items-center rounded-lg py-3 px-6 mb-5">
+       <img src="https://cdn-images-1.medium.com/max/606/1*rOPLUJ3W6FUA3rO1U1IeuA@2x.png" alt="netflix technology blog rss feed" />
+     </div>
+     {articles.map((item, i) =>
+    <Feed 
+    key={i}
+    title={item.item.title}
+    link={item.item.link}
+    date={item.item.pubDate}
+    />
+    )}
     </div>
+    </>
   );
 }
 
