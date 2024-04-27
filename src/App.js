46ee6e7ff5341f6542e1fcf6cd5c9f5256595df2
heshaniadithya <heshaniadithya2006@gmail.com>
2024-04-28 00:43:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <>
       <h1 className="text-xl font-semibold text-center mt-5">05 - RSS Feed</h1>
-      <h2 className="text-3xl font-semibold text-center mt-2 mb-4">Good morning, Heshani</h2>
+      <h2 className="text-3xl font-semibold text-center mt-2 mb-4">Good Morning, Heshani</h2>
       <div className="w-3/4 max-w-lg border mx-auto p-5 rounded-xl">
      <div className="bg-black flex flex-col items-center rounded-lg py-3 px-6 mb-5">
        <img src="https://cdn-images-1.medium.com/max/606/1*rOPLUJ3W6FUA3rO1U1IeuA@2x.png" alt="netflix technology blog rss feed" />
